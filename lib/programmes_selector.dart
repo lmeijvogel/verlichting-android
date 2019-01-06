0ca46a024b6c6d389c005da0d2460ae92532521f
Lennaert Meijvogel <l.meijvogel@yahoo.co.uk>
2019-01-06 16:59:52 +0100

    Load programmes and current programme on load. Show spinner.

--- lib/programmes_selector.dart
+++ lib/programmes_selector.dart
@@ -13,8 +13,16 @@ class ProgrammesSelectorWidget extends StatefulWidget {
 }
 
 class _ProgrammesSelectorState extends State<ProgrammesSelectorWidget> {
+  bool _loading = false;
   List<Programme> _programmes;
   Programme _currentProgramme;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _loadProgrammes().then((_) { _loadCurrentProgramme(); });
+  }
 
   void _programmesLoaded(List<Programme> programmes) {
     setState(() {
@@ -24,14 +32,21 @@ class _ProgrammesSelectorState extends State<ProgrammesSelectorWidget> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _programmes = programmes;
+      _loading = false;
     });
   }
 
-  _loadProgrammes() {
-    String basicAuth =
-        'Basic ' + base64Encode(utf8.encode('${CONNECTION_INFO.username}:${CONNECTION_INFO.password}'));
+  Future<void> _loadProgrammes() {
+    setState(() {
+      _loading = true;
+    });
 
-    http.get(CONNECTION_INFO.host + "/available_programmes", headers: {HttpHeaders.authorizationHeader: basicAuth}).then((response) {
+    String basicAuth = 'Basic ' +
+        base64Encode(utf8
+            .encode('${CONNECTION_INFO.username}:${CONNECTION_INFO.password}'));
+
+    return http.get(CONNECTION_INFO.host + "/available_programmes",
+        headers: {HttpHeaders.authorizationHeader: basicAuth}).then((response) {
       List<Programme> programmes = [];
 
       var programmesJson = jsonDecode(response.body)["availableProgrammes"];
@@ -42,37 +57,64 @@ class _ProgrammesSelectorState extends State<ProgrammesSelectorWidget> {
 
       _programmesLoaded(programmes);
     });
+
+  }
+
+  _currentProgrammeLoaded(String programmeId) {
+    setState(() {
+      _currentProgramme = _programmes.firstWhere((programme) { return programme.id == programmeId; });
+    });
+  }
+
+  _loadCurrentProgramme() {
+    String basicAuth = 'Basic ' +
+        base64Encode(utf8
+            .encode('${CONNECTION_INFO.username}:${CONNECTION_INFO.password}'));
+
+    http.get(CONNECTION_INFO.host + "/current_programme",
+        headers: {HttpHeaders.authorizationHeader: basicAuth}).then((response) {
+
+          var programmeJson = jsonDecode(response.body);
+
+          _currentProgrammeLoaded(programmeJson["programme"]);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     var contents = _programmeButtons();
 
-    contents.add(new RaisedButton(onPressed: _loadProgrammes, child: new Text("Load programmes")));
+    contents.add(new RaisedButton(
+        onPressed: _loadProgrammes, child: new Text("Reload programmes")));
 
-    return Center(
-      // Center is a layout widget. It takes a single child and positions it
-      // in the middle of the parent.
-      child: Column(
-        // Column is also layout widget. It takes a list of children and
-        // arranges them vertically. By default, it sizes itself to fit its
-        // children horizontally, and tries to be as tall as its parent.
-        //
-        // Invoke "debug painting" (press "p" in the console, choose the
-        // "Toggle Debug Paint" action from the Flutter Inspector in Android
-        // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-        // to see the wireframe for each widget.
-        //
-        // Column has various properties to control how it sizes itself and
-        // how it positions its children. Here we use mainAxisAlignment to
-        // center the children vertically; the main axis here is the vertical
-        // axis because Columns are vertical (the cross axis would be
-        // horizontal).
+    if (_loading) {
+      return Center(
+        child: CircularProgressIndicator()
+      );
+    } else {
+      return Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: contents,
-      ),
-
-    );
+        ),
+      );
+    }
   }
 
   List<Widget> _programmeButtons() {
@@ -81,28 +123,44 @@ class _ProgrammesSelectorState extends State<ProgrammesSelectorWidget> {
     }
 
     return _programmes.map((programme) {
-      var active = _currentProgramme != null && (programme.id == _currentProgramme.id);
+      var active =
+          _currentProgramme != null && (programme.id == _currentProgramme.id);
 
       if (active) {
-        return RaisedButton(onPressed: () {
-          _programmeSelected(programme);
-        }, child: Text(programme.name)
-          ,  color: Colors.blue
-          ,  textColor: Colors.white,);
+        return RaisedButton(
+          onPressed: () {
+            _programmeSelected(programme);
+          },
+          child: Text(programme.name),
+          color: Colors.blue,
+          textColor: Colors.white,
+        );
       } else {
-        return FlatButton(onPressed: () {
-          _programmeSelected(programme);
-        }, child: Text(programme.name),);
+        return FlatButton(
+          onPressed: () {
+            _programmeSelected(programme);
+          },
+          child: Text(programme.name),
+        );
       }
     }).toList();
   }
 
   _programmeSelected(Programme programme) {
-    setState(() {
-      _currentProgramme = programme;
+    var url = "/programme/${programme.id}/start";
+
+    String basicAuth = 'Basic ' +
+        base64Encode(utf8
+            .encode('${CONNECTION_INFO.username}:${CONNECTION_INFO.password}'));
+
+    http.post(CONNECTION_INFO.host + url,
+        headers: {HttpHeaders.authorizationHeader: basicAuth}).then((response) {
+
+      setState(() {
+        _currentProgramme = programme;
+      });
     });
   }
-
 }
 
 class Programme {
