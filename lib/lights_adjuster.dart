13332bddf5ed797b0f91a4c692a79af09176bec2
Lennaert Meijvogel <l.meijvogel@yahoo.co.uk>
2019-01-09 21:10:20 +0100

    Extract Light class to separate file.

--- lib/lights_adjuster.dart
+++ lib/lights_adjuster.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:verlichting/authenticated_request.dart';
+import 'package:verlichting/models/light.dart';
 
 final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey = new GlobalKey();
 
@@ -134,107 +135,4 @@ class _LightsAdjusterState extends State<LightsAdjusterWidget> {
       light.waiting = false;
     });
   }
-}
-
-class DimmableLight extends Light {
-  num _oldValue;
-
-  DimmableLight(
-    num nodeId,
-    String name,
-    String displayName,
-    num value,
-  ) : super(nodeId, name, displayName, false, value) {
-    _oldValue = value;
-  }
-
-  @override
-  bool isOn() => value > 0;
-
-  @override
-  Future<NewLightState> toggle(bool newState) {
-    var newValue;
-
-    if (newState) {
-      newValue = _oldValue > 0 ? _oldValue : 2;
-    } else {
-      _oldValue = value;
-      newValue = 0;
-    }
-
-    return AuthenticatedRequest.post("/light/${nodeId}/level/$newValue")
-        .then((response) {
-      var levelFromServer = jsonDecode(response.body)["level"];
-
-      value = levelFromServer;
-
-      return NewLightState(false, value);
-    }).catchError((error) {
-      print(error);
-    });
-  }
-
-  @override
-  applyState(NewLightState newLightState) {
-    state = newLightState.state;
-  }
-}
-
-class SwitchableLight extends Light {
-  SwitchableLight(
-    num nodeId,
-    String name,
-    String displayName,
-    bool state,
-  ) : super(nodeId, name, displayName, state, 0);
-
-  @override
-  bool isOn() => state;
-
-  @override
-  Future<NewLightState> toggle(bool newState) {
-    return AuthenticatedRequest.post("/light/$nodeId/switch/$newState")
-        .then((response) {
-      var stateFromServer = jsonDecode(response.body)["state"];
-
-      this.state = stateFromServer;
-
-      return NewLightState(state, 0);
-    });
-  }
-
-  @override
-  applyState(NewLightState newLightState) {
-    state = newLightState.state;
-  }
-}
-
-abstract class Light {
-  num nodeId;
-  String name;
-  String displayName;
-  bool state;
-  num value;
-  bool waiting = false;
-
-  Light(
-    this.nodeId,
-    this.name,
-    this.displayName,
-    this.state,
-    this.value,
-  );
-
-  bool isOn();
-
-  Future<NewLightState> toggle(bool newState);
-
-  applyState(NewLightState newLightState);
-}
-
-class NewLightState {
-  bool state;
-  num value;
-
-  NewLightState(this.state, this.value);
 }
